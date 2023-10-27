7cf617d9d90002380eccdb17f8a7098027c1f9a8
KhunEss <sakda.ace@gmail.com>
2023-10-27 16:25:21 +0700

    init

---
+++ readme.md
@@ -0,0 +1,105 @@
+# EzqMongo
+
+EzqMongo is a simple event-driven queue processing library for Node.js that is built on top of Mongoose and EventEmitter. It provides a way to manage and process queues in a MongoDB database with ease. This README provides an overview of the library and how to use it in your Node.js applications.
+
+## Installation
+
+```bash
+npm install eazy-queue-mongo-ks
+```
+
+### Import
+
+commonJS:
+
+```javascript
+const EzqMongo = require('eazy-queue-mongo-ks');
+```
+
+es2015:
+
+```javascript
+import EzqMongo from 'eazy-queue-mongo-ks';
+```
+
+## API
+
+### `new EzqMongo(queueName?: string, stackLimit?: number)`
+
+- `queueName` (optional): The name of the queue. Default is 'ezq'.
+- `stackLimit` (optional): The maximum number of items to process at once. Default is 1.
+
+### `add(data: T | T[])`
+
+Add data to the queue. You can pass a single item or an array of items.
+
+### `process(callback: (data: T | undefined) => Promise<void>)`
+
+The `process` method is used to process items in the queue by invoking the provided callback function. It processes items one by one until the queue is empty or until an error occurs.
+
+### `checkQueue()`
+
+The `checkQueue` method is used to check the number of items in the queue. It returns a promise that resolves to the count of items in the queue.
+
+### `clearQueue()`
+
+Clear the entire queue. Use this with caution, as it removes all data in the queue.
+
+### Events
+
+EzqMongo emits the following events that you can listen for:
+
+- `job`: Triggered when a job is available for processing.
+- `done`: Triggered after processing a job. Provides the processing result and the processed data.
+- `complete`: Triggered when the entire queue processing is complete.
+
+## Usage
+
+Here's a basic example of how to use EzqMongo in your Node.js application:
+
+```javascript
+import EzqMongo from 'eazy-queue-mongo-ks';
+import mongoose from 'mongoose';
+
+mongoose.connect('your-mongodb/your-db');
+
+// Create an instance of EzqMongo with a custom queue name and an optional stack limit
+const ezqMongo = new EzqMongo('myQueue', 10);
+
+// Listen for events
+ezqMongo.on('job', () => {
+  // Process the queue with the defined callback
+  ezqMongo.process(async (job) => {
+    await somethingAsync(job);
+    // ...TODO
+    return `ok ${job}`; // Not require return
+  });
+});
+
+// Add data to the queue
+ezqMongo.add({ name: 'foo' });
+// Or
+ezqMongo.add([{ name: 'foo' }, { name: 'bar' }]);
+
+ezqMongo.on('done', (result, data) => {
+  // If ezq.process is return ezqMongo.process. example return `ok {name: 'foo'}`
+  // Job is data of process. example {name: 'foo'}
+  console.log('Done :', result, data);
+});
+
+ezqMongo.on('complete', () => {
+  // All job is done
+  console.log('Queue processing complete');
+});
+
+// Clear the queue (use with caution, as it removes all data in the queue) Mongo deleteMany with queueName
+ezqMongo.clearQueue();
+```
+
+## License
+
+This library is provided under the [MIT License](LICENSE). Feel free to use and modify it in your projects.
+
+## Issues and Contributions
+
+If you encounter any issues or have suggestions for improvements, please open an issue on the [GitHub repository](https://github.com/suzumi987/eazy-queue-mongo-ks.git).
---
+++ src/index.ts
@@ -0,0 +1,143 @@
+import { EventEmitter } from 'events';
+import mongoose, { Schema } from 'mongoose';
+
+const qSchema = new Schema({
+  queue_name: String,
+  data: mongoose.SchemaTypes.Mixed,
+  created: Date,
+});
+
+const qModel = mongoose.model('ezqueue', qSchema, 'ezqueue');
+
+interface IQueue<T> {
+  _id: mongoose.Types.ObjectId;
+  queue_name: string;
+  data: T;
+  created: Date;
+}
+
+class EzqMongo<T> extends EventEmitter {
+  private isProcess = false;
+  private isQueue = false;
+  private pause = false;
+  private queue: IQueue<T>[];
+
+  constructor(
+    private queueName: string = 'ezq',
+    private stackLimit: number = 1,
+  ) {
+    super();
+    this.queue = [];
+    this.ckQ();
+  }
+
+  private async ckQ() {
+    this.pause = true;
+    const queueCount = await this.checkQueue();
+    this.isQueue = queueCount > 0;
+
+    if (this.isQueue) {
+      this.isProcess = true;
+      this.emit('job');
+    }
+
+    if (queueCount === 0) {
+      this.isQueue = false;
+    }
+
+    this.pause = false;
+  }
+
+  public async checkQueue() {
+    const [agg] = await qModel.aggregate([
+      {
+        $match: {
+          queue_name: this.queueName,
+        },
+      },
+      {
+        $sort: {
+          created: 1,
+        },
+      },
+      {
+        $facet: {
+          queue: [
+            { $limit: this.stackLimit },
+            {
+              $project: {
+                queue_name: 1,
+                data: 1,
+                created: 1,
+              },
+            },
+          ],
+          count: [{ $count: 'count' }],
+        },
+      },
+      {
+        $project: {
+          queue: 1,
+          c: {
+            $ifNull: [{ $arrayElemAt: ['$count', 0] }, { count: 0 }],
+          },
+        },
+      },
+    ]);
+
+    const { c, queue }: { c: { count: number }; queue: IQueue<T>[] } = agg;
+    if (c.count && this.queue.length === 0) {
+      this.queue = queue;
+      const mapId = queue.map((f: IQueue<T>) => f._id);
+      await qModel.deleteMany({ _id: { $in: mapId } });
+    }
+    return c.count;
+  }
+
+  public async add(data: T | T[]) {
+    if (Array.isArray(data) && data.length > 1000) {
+      throw new Error('Data array cannot be more than 100');
+    }
+    const dataArray = Array.isArray(data) ? data : [data];
+    const ins = dataArray.map((d) => ({
+      queue_name: this.queueName,
+      data: d,
+      created: new Date(),
+    }));
+    await qModel.insertMany(ins);
+
+    if (!this.isProcess && !this.pause) {
+      await this.checkQueue();
+      this.isQueue = true;
+      this.emit('job');
+    }
+  }
+
+  public async process(callback: (data: T | undefined) => Promise<void>) {
+    this.isProcess = true;
+
+    while (this.queue.length > 0) {
+      const { data: job } = this.queue.shift() || { data: undefined };
+      const result = await callback(job);
+      this.emit('done', result, job);
+    }
+    const c = await this.checkQueue();
+    if (c === 0) {
+      this.isQueue = false;
+      this.emit('complete');
+      this.isProcess = false;
+    } else {
+      this.emit('job');
+    }
+  }
+
+  public async clearQueue() {
+    this.pause = true;
+    await qModel.deleteMany({ queue_name: this.queueName });
+    this.isQueue = false; // Ensure isQueue is set to false when the queue is cleared
+    this.isProcess = false;
+    this.pause = false;
+  }
+}
+
+export default EzqMongo;
